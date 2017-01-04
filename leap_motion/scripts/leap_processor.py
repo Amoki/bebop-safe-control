65329b95d1edec3618c8fa1c3c752e1c5d005f8f
viki <paul-ristori@hotmail.fr>
2017-01-04 10:19:11 -0600

    add leap-motion

---
+++ leap_motion/scripts/leap_processor.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+__author__ = 'flier'
+
+import argparse
+
+import rospy
+import datetime
+import leap_interface
+from leap_motion.msg import leap
+from leap_motion.msg import leapros
+from geometry_msg.msg import Twist
+from geometry_msg.msg import Vector3
+
+NODENAME = 'hand_vect'
+
+def init():
+    time_mem=0
+
+    leap_origin.x=0
+    leap_origin.y=0
+    leap_origin.z=0
+
+    Twist_msg = Twist()
+    Twist_msg.target_pos.x=0
+    Twist_msg.target_pos.y=0
+    Twist_msg.target_pos.z=0
+
+    Twist_msg.action.loop=0
+    Twist_msg.action.takeoff=0
+    Twist_msg.action.land=0
+
+    old_hand_pos.x=0
+    old_hand_pos.y=0
+    old_hand_pos.z=0
+
+    com_relative_mode=1
+
+
+def listener_callback(data):
+    
+    pub = rospy.Publisher('leapmotion/data',Twist,queue_size=1)
+    rospy.Rate(40)
+    rospy.init_node(NODENAME,anonymous=True)
+    
+    '''if start position initialized'''
+    '''TO DO'''
+
+    if com_relative_mode:
+        delta_pos.x= data.palmpos.x - old_hand_pos.x
+        delta_pos.y= data.palmpos.y - old_hand_pos.y
+        delta_pos.z= data.palmpos.z - old_hand_pos.z
+
+        delta_time = datetime.datetime.now() - time_mem
+        time_mem = datetime.datetime.now()
+
+        time_mem = datetime.datetime.now()
+
+        linear_vector = Vector3(
+            x = (delta_pos.x/delta_time), #left/right
+            y = (delta_pos.y/delta_time), #upward/downward
+            z = (delta_pos.z/delta_time) #forward/rearward
+        )
+
+        angular_vector = Vector3(
+            x=0,
+            y=0,
+            z=0
+        )
+
+        message = Twist(linear=linear_vector, angular_vector=angular_vector)
+        
+        pub.publish(message)
+
+
+
+
+def listener():
+    init()
+    rospy.init_node('leap_sub', anonymous=True)
+    # rospy.Subscriber("leapmotion/raw", leap, callback)
+    rospy.Subscriber("leapmotion/data", leapros, listener_callback)
+    rospy.spin()
+
+    if __name__ == '__main__':
+        try:
+            listener()
+        except rospy.ROSInterruptException:
+            pass
---
+++ leap_motion/scripts/subscriber.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+__author__ = 'flier'
+
+import rospy
+from leap_motion.msg import leap
+from leap_motion.msg import leapros
+
+# Native datatypes, I've heard this is bad practice, use the geometry messages instead.
+# def callback(data):
+#    rospy.loginfo(rospy.get_name() + ": Leap Raw Data %s" % data)
+
+
+# Callback of the ROS subscriber, just print the received data.
+def callback_ros(data):
+    #rospy.loginfo(rospy.get_name() + ": Leap ROS Data %s" % data)
+    #string = """
+    #{jdehfvn}
+    #""".format(
+    #   jdehfvn= data.azerty
+    #)
+    print("Palm\t\tdirection[%s,%s,%s]\tnormal[%s,%s,%s]\tpalmpos[%s,%s,%s]\typr[%s,%s,%s]\t\n" % (
+        data.direction.x,data.direction.y,data.direction.z, 
+        data.normal.x,data.normal.y,data.normal.z,
+        data.palmpos.x,data.palmpos.y,data.palmpos.z,
+        data.ypr.x,data.ypr.y,data.ypr.z))
+    print("Thumb\t\tmetacarpal[%s,%s,%s]\tproximal[%s,%s,%s]\tintermediate[%s,%s,%s]\tdistal[%s,%s,%s]\ttip[%s,%s,%s]\t\n" % (
+        data.thumb_metacarpal.x,data.thumb_metacarpal.y,data.thumb_metacarpal.z,
+        data.thumb_proximal.x,data.thumb_proximal.y,data.thumb_proximal.z,
+        data.thumb_intermediate.x,data.thumb_intermediate.y,data.thumb_intermediate.z,
+        data.thumb_distal.x,data.thumb_distal.y,data.thumb_distal.z,
+        data.thumb_tip.x,data.thumb_tip.y,data.thumb_tip.z))
+
+    print("Index\t\tmetacarpal[%s,%s,%s]\tproximal[%s,%s,%s]\tintermediate[%s,%s,%s]\tdistal[%s,%s,%s]\ttip[%s,%s,%s]\t\n" % (
+        data.index_metacarpal.x,data.index_metacarpal.y,data.index_metacarpal.z,
+        data.index_proximal.x,data.index_proximal.y,data.index_proximal.z,
+        data.index_intermediate.x,data.index_intermediate.y,data.index_intermediate.z,
+        data.index_distal.x,data.index_distal.y,data.index_distal.z,
+        data.index_tip.x,data.index_tip.y,data.index_tip.z))
+
+    print("Middle\t\tmetacarpal[%s,%s,%s]\tproximal[%s,%s,%s]\tintermediate[%s,%s,%s]\tdistal[%s,%s,%s]\ttip[%s,%s,%s]\t\n" % (
+        data.middle_metacarpal.x,data.middle_metacarpal.y,data.middle_metacarpal.z,
+        data.middle_proximal.x,data.middle_proximal.y,data.middle_proximal.z,
+        data.middle_intermediate.x,data.middle_intermediate.y,data.middle_intermediate.z,
+        data.middle_distal.x,data.middle_distal.y,data.middle_distal.z,
+        data.middle_tip.x,data.middle_tip.y,data.middle_tip.z))
+
+    print("Ring\t\tmetacarpal[%s,%s,%s]\tproximal[%s,%s,%s]\tintermediate[%s,%s,%s]\tdistal[%s,%s,%s]\ttip[%s,%s,%s]\t\n" % (
+        data.ring_metacarpal.x,data.ring_metacarpal.y,data.ring_metacarpal.z,
+        data.ring_proximal.x,data.ring_proximal.y,data.ring_proximal.z,
+        data.ring_intermediate.x,data.ring_intermediate.y,data.ring_intermediate.z,
+        data.ring_distal.x,data.ring_distal.y,data.ring_distal.z,
+        data.ring_tip.x,data.ring_tip.y,data.ring_tip.z))
+    
+    print("Pinky\t\tmetacarpal[%s,%s,%s]\tproximal[%s,%s,%s]\tintermediate[%s,%s,%s]\tdistal[%s,%s,%s]\ttip[%s,%s,%s]\t\n" % (
+        data.pinky_metacarpal.x,data.pinky_metacarpal.y,data.pinky_metacarpal.z,
+        data.pinky_proximal.x,data.pinky_proximal.y,data.pinky_proximal.z,
+        data.pinky_intermediate.x,data.pinky_intermediate.y,data.pinky_intermediate.z,
+        data.pinky_distal.x,data.pinky_distal.y,data.pinky_distal.z,
+        data.pinky_tip.x,data.pinky_tip.y,data.pinky_tip.z))
+
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+# Yes, a listener aka subscriber ;) obviously. Listens to: leapmotion/data
+def listener():
+    rospy.init_node('leap_sub', anonymous=True)
+    # rospy.Subscriber("leapmotion/raw", leap, callback)
+    rospy.Subscriber("leapmotion/data", leapros, callback_ros)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
