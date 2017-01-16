6e0f40050f68f3efa172f71669893b05b52907d3
MePec <pe.cochet.38@gmail.com>
2017-01-16 17:51:47 +0100

    Code: connected to COM, but cant do remotepositioning

--- pozyx/arduino/arduinorospozyx/arduinorospozyx.ino
+++ pozyx/arduino/arduinorospozyx/arduinorospozyx.ino
@@ -6,7 +6,7 @@
  * It send the different values into a ROS topic
  *
  * @author: Pierre-Emmanuel Cochet
- * @date :  december 6th 2017
+ * @date :  january 6th 2017
  */
 
 #include <ArduinoHardware.h>
@@ -17,14 +17,15 @@
 //#include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/String.h>
 
+#define USB_CON
 ///////////////// PARAMETERS ////////////////
 /* Pozyx */
 uint8_t   n_tags       = 1;
 uint16_t  flyingTags[1]= {0x683D};
 uint16_t  masterTag[1] = {0x6F4A};
 
-uint8_t   n_anchors   = 3;
-uint16_t  anchors[4]  = { 0x606B, 0x603B, 0x6037, 0x683D};  // THIS IS HARD
+uint8_t   n_anchors   = 4;
+uint16_t  anchors[4]  = { 0x606B, 0x603B, 0x6037, 0x6F4A};  // THIS IS HARD
 int32_t   anchors_x[4]= { 2000,   2000,   3600,   0000  };  // CODDED. YOU
 int32_t   anchors_y[4]= { 3200,   2000,   3600,   0000  };  // MUST CHANGE (mm)
 int32_t   anchors_z[4]= { 0600,   0600,   0000,   0000  };  // FOR YOUR APP
@@ -36,12 +37,16 @@ ros::NodeHandle  nh;
 
 //geometry_msgs::PoseStamped geo_pos;
 std_msgs::String geo_pos;
+std_msgs::String msg_pos;
 
 ros::Publisher pub_pos("pos",  &geo_pos);
 
 /////////////////////////////////////////////
 void setup()
 {
+  nh.initNode();
+  nh.advertise(pub_pos);
+
   if(Pozyx.begin() == POZYX_FAILURE){
     delay(100);
     abort();
@@ -49,9 +54,6 @@ void setup()
   Pozyx.clearDevices(NULL);
   setAnchorsManual();
   delay(2000);
-
-  nh.initNode();
-  nh.advertise(pub_pos);
 }
 
 /////////////////////////////////////////////
@@ -60,7 +62,8 @@ void loop()
   coordinates_t position;
 
   for (int i = 0; i < n_tags; i++){
-    int status = Pozyx.doRemotePositioning(flyingTags[i], &position, dimension, 0, POZYX_POS_ALG_UWB_ONLY);
+    int status = Pozyx.doRemotePositioning(flyingTags[0], &position, dimension,0 ,POZYX_POS_ALG_UWB_ONLY);
+    
     if (status == POZYX_SUCCESS){
       // prints out the result
       printCoordinates(flyingTags[i], position);
@@ -69,8 +72,6 @@ void loop()
       printErrorCode("pos", flyingTags[i]);
     }
   }
-
-  pub_pos.publish( &geo_pos );
   nh.spinOnce();
   delay(1000);
 }
@@ -79,7 +80,7 @@ void loop()
 ///////////////// FONCTIONS /////////////////
 /*Function that set the anchors*/
 void setAnchorsManual(){
-  
+
   device_coordinates_t anchor;
   for(int i = 0; i < n_anchors; i++){
     anchor.network_id = anchors[i];
@@ -87,9 +88,13 @@ void setAnchorsManual(){
     anchor.pos.x = anchors_x[i];
     anchor.pos.y = anchors_y[i];
     anchor.pos.z = anchors_z[i];
-    Pozyx.addDevice(anchor, masterTag[0]);
+    if (POZYX_SUCCESS == Pozyx.addDevice(anchor, NULL)){
+      // OK
+    }else{
+      // configuration Fail
+    }
  }
- 
+
 }
 
 /*Function that print the Error Code*/
@@ -97,26 +102,32 @@ void printErrorCode(String operation, uint16_t network_id){
   uint8_t error_code;
   int status = Pozyx.getErrorCode(&error_code, network_id);
   if(status == POZYX_SUCCESS){
-   /* Serial.print("ERROR ");
+    /*Serial.print("ERROR ");
     Serial.print(operation);
     Serial.print(" on ID 0x");
     Serial.print(network_id, HEX);
     Serial.print(", error code: 0x");
     Serial.println(error_code, HEX);*/
+    sprintf(msg_pos.data, "ERROR id:%X, error code:%X", network_id, error_code);
+    pub_pos.publish( &msg_pos );
+
   }else{
     Pozyx.getErrorCode(&error_code, NULL);
     /*Serial.print("ERROR ");
     Serial.print(operation);
     Serial.print(", couldn't retrieve remote error code, local error: 0x");
     Serial.println(error_code, HEX);*/
+    sprintf(msg_pos.data, "ERROR unknown id, error code:%X", error_code);
+    pub_pos.publish( &msg_pos );
   }
 }
 
 /*Function that print the coordinates*/
 void printCoordinates(uint16_t tag_id, coordinates_t position){
-  //char mik[12];
+  char hello[13] = "hello world!";
+  char mik[75];
+  //sprintf(mik, "id: 0x%X - pos: %d ", tag_id, position.x);
+  sprintf(geo_pos.data, "id: 0x%X - pos: %d ", tag_id, position.x);
   //geo_pos.data = mik;
-  sprintf(geo_pos.data, "id: 0x%X - pos: %d", tag_id, position.x);
   pub_pos.publish( &geo_pos );
-  nh.spinOnce();
   }
