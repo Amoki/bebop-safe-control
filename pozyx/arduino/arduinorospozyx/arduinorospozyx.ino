7450b07fd9b453fa41e93f61c6c161f58e42a62b
MePec <pe.cochet.38@gmail.com>
2017-01-19 18:41:27 +0100

    Works for 1 tag, not for 2

--- pozyx/arduino/arduinorospozyx/arduinorospozyx.ino
+++ pozyx/arduino/arduinorospozyx/arduinorospozyx.ino
@@ -17,19 +17,18 @@
 #include <std_msgs/String.h>
 
 ///////////////// PARAMETERS ////////////////
-
 #define USB_CON
-#define NB_TAGS 1
+#define NB_TAGS 2
+#define NB_ANCH 4
 
 /* Pozyx */
-uint16_t  flyingTags[NB_TAGS]= {0x683D};
+uint16_t  flyingTags[NB_TAGS]= {0x606B, 0x683D};
 uint16_t  masterTag = 0x6F4A;
 
-uint8_t   n_anchors   = 4;
-uint16_t  anchors[4]  = { 0x606B, 0x603B, 0x6037, 0x6F4A};  // THIS IS HARD
-int32_t   anchors_x[4]= { 2900, 0000, 2900, 0000};  // CODDED. YOU
-int32_t   anchors_y[4]= { 7900, 0000, 0000, 7900};  // MUST CHANGE (mm)
-int32_t   anchors_z[4]= { 0000, 1650, 1050, 1050};  // FOR YOUR APP
+uint16_t  anchors[NB_ANCH]  = { 0x6001, 0x603A, 0x6037, 0x603B};  // THIS IS HARD
+int32_t   anchors_x[NB_ANCH]= { 2900, 0000, 2900, 0000};          // CODDED. YOU
+int32_t   anchors_y[NB_ANCH]= { 7900, 0000, 0000, 7900};          // MUST CHANGE (mm)
+int32_t   anchors_z[NB_ANCH]= { 0000, 1650, 1050, 1050};          // FOR YOUR APP : TEST
 
 uint8_t   dimension   = POZYX_3D;
 
@@ -37,25 +36,21 @@ uint8_t   dimension   = POZYX_3D;
 ros::NodeHandle  nh;
 
 std_msgs::String geo_pos;
-std_msgs::String msg_pos;
 
 ros::Publisher pub_pos("pos",  &geo_pos);
-//ros::Publisher pub_mdg("pos",  &msg_pos);
-
 
 /////////////////////////////////////////////
 void setup()
 {
   nh.initNode();
-  nh.advertise(pub_pos); 
+  nh.advertise(pub_pos);
 
   if(Pozyx.begin() == POZYX_FAILURE){
     delay(100);
     abort();
   }
-  clearDevices();
+  //setMaster();
   setAnchorsManual();
-  setTagsManual();
   delay(2000);
   
 }
@@ -64,7 +59,6 @@ void setup()
 void loop()
 {
   coordinates_t position;
-
   for (int i = 0; i < NB_TAGS; i++){
     int status = Pozyx.doRemotePositioning(flyingTags[i], &position, dimension, 0,POZYX_POS_ALG_UWB_ONLY );
     
@@ -75,7 +69,6 @@ void loop()
       // prints out the error code
       printErrorCode("pos", flyingTags[i]);
     }
-    nh.spinOnce();
   }
 }
 
@@ -83,70 +76,55 @@ void loop()
 ///////////////// FONCTIONS /////////////////
 /*Function that set the anchors*/
 void setAnchorsManual(){
-
-  device_coordinates_t anchor;
-  for(int i = 0; i < n_anchors; i++){
-    anchor.network_id = anchors[i];
-    anchor.flag = 1;
-    anchor.pos.x = anchors_x[i];
-    anchor.pos.y = anchors_y[i];
-    anchor.pos.z = anchors_z[i];
-
-    Pozyx.setOperationMode(POZYX_ANCHOR_MODE,anchors[i]);
-
-    for(int i = 0; i < NB_TAGS; i++){
-      if (POZYX_SUCCESS == Pozyx.addDevice(anchor, flyingTags[0])){
-      // OK
-      }
-     }
-  }
-
-}
-
-/*Function that set the flying tags*/
-void setTagsManual(){
-
-  for(int i = 0; i < NB_TAGS; i++){
-    if (POZYX_SUCCESS == Pozyx.setOperationMode(POZYX_TAG_MODE,flyingTags[i])){
-      // OK
-    }
-  }
-
-}
-
-/*Function that init all the devices*/
-void clearDevices(){
-  for(int i = 0; i < n_anchors; i++){
-    Pozyx.clearDevices(anchors[i]);
-  }
-  for(int i = 0; i < NB_TAGS; i++){
-    Pozyx.clearDevices(flyingTags[i]);
-  }
+  int i, j = 0;
+  int status;
   
- }
+  for( j = 0; j < NB_ANCH; j++){
+     status &= Pozyx.setOperationMode(POZYX_ANCHOR_MODE,anchors[j]);
+     }
+  for ( i = 0; i < NB_TAGS; i++){
+    status &= Pozyx.clearDevices(flyingTags[i]);
+    status &= Pozyx.setOperationMode(POZYX_TAG_MODE,flyingTags[i]);
+      for( j = 0; j < NB_ANCH; j++){
+        device_coordinates_t anchor;
+        anchor.network_id = anchors[j];
+        anchor.flag = 1;
+        anchor.pos.x = anchors_x[j];
+        anchor.pos.y = anchors_y[j];
+        anchor.pos.z = anchors_z[j];
  
+        status &= Pozyx.addDevice(anchor, flyingTags[i]);
+      }
+  }
+  //if(){}else{}
+}
+
+/*Function to set the master*/
+void setMaster(){
+  Pozyx.clearDevices(masterTag);
+  Pozyx.doDiscovery(POZYX_DISCOVERY_ALL_DEVICES, NB_TAGS + NB_ANCH, 20);
+}
+
 /*Function that print the Error Code*/
 void printErrorCode(String operation, uint16_t network_id){
   uint8_t error_code;
   int status = Pozyx.getErrorCode(&error_code, network_id);
   
   if(status == POZYX_SUCCESS){
-    sprintf(msg_pos.data, "ERROR id:%X, error code:%X", network_id, error_code);
-    pub_pos.publish( &msg_pos );
-
+    sprintf(geo_pos.data, "ERROR id:%X, error code:%X", network_id, error_code);
+    pub_pos.publish( &geo_pos );
+    nh.spinOnce();
   }else{
     Pozyx.getErrorCode(&error_code, NULL);
-    sprintf(msg_pos.data, "ERROR unknown id, error code:%X", error_code);
-    pub_pos.publish( &msg_pos );
+    sprintf(geo_pos.data, "ERROR unknown id, error code:%X", error_code);
+    pub_pos.publish( &geo_pos );
+    nh.spinOnce();
   }
 }
 
 /*Function that print the coordinates*/
 void printCoordinates(uint16_t tag_id, coordinates_t* position){
-  char mik[50];
-  //sprintf(mik, "id: 0x%X - pos: %d ", tag_id, position.x);
   sprintf(geo_pos.data, "id: 0x%X - pos: %i  %i  %i", tag_id, int(position->x),  int(position->y),  int(position->z));
-
-  //geo_pos.data = mik;
   pub_pos.publish( &geo_pos );
+  nh.spinOnce();
 }
